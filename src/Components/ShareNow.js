51242216a782500df084d61e6ee08429c0135497
soubhagya23 <ranjansoubhagya.master@gmail.com>
2021-09-10 21:39:07 +0530

    upi testing

--- src/Components/ShareNow.js
+++ src/Components/ShareNow.js
@@ -1,34 +1,42 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-import image from "./shareImage.jpeg"
-import video from "./TestVideo.mp4"
+import image from "./shareImage.jpeg";
+import video from "./TestVideo.mp4";
 
 export default function ShareNow() {
-  const handleOnSubmit= async()=> {
+  const handleOnSubmit = async () => {
     const response = await fetch(video);
-    
     const blob = await response.blob();
-    const file = new File([blob], 'share.mp4', {type: blob.type});
+    const file = new File([blob], "share.mp4", { type: blob.type });
+
+    /*  If Image 
+    const response = await fetch(image);
+    const blob = await response.blob();
+    const file = new File([blob], 'share.jpg', {type: blob.type}); */
+
     console.log(file);
-    if(navigator.share) {
-      await navigator.share({
-        title: "title",
-        text: "Mobilly Invite",
-        url: "https://mobillyinvite.com/",
-        files: [file]     
-      })
-        .then(() => console.log('Successful share'))
-        .catch((error) => console.log('Error in sharing', error));
-    }else {
+    if (navigator.share) {
+      await navigator
+        .share({
+          title: "title",
+          text: "Mobilly Invite",
+          url: "https://mobillyinvite.com/",
+          files: [file],
+        })
+        .then(() => console.log("Successful share"))
+        .catch((error) => console.log("Error in sharing", error));
+    } else {
       console.log(`system does not support sharing files.`);
     }
-  }
-  
-  useEffect(()=> {
+  };
+
+  useEffect(() => {
     if (navigator.share === undefined) {
-      if (window.location.protocol === 'http:') {
-        window.location.replace(window.location.href.replace(/^http:/, 'https:'));
-      } 
+      if (window.location.protocol === "http:") {
+        window.location.replace(
+          window.location.href.replace(/^http:/, "https:")
+        );
+      }
     }
   }, []);
   return (
@@ -41,6 +49,7 @@ export default function ShareNow() {
       >
         Share
       </button>
+      <a href="upi://pay?pa=9777758789@ybl&amp;pn=SOUBHAGYA RANJAN &amp;cu=INR" class="upi-pay1">Pay Now !</a>
     </div>
   );
 }
